fd1bf437df8c174af3037a554e80a11fbcaade0e
Devi Nur Azizah <devinurazizah27gmail.com>
2021-04-01 13:41:13 +0700

    tugas kelima

---
+++ Numbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return 3 - 2;
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return 5 - 3;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return 2 + 1;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return 5 - 1;
+        }
+    }
+
+}
